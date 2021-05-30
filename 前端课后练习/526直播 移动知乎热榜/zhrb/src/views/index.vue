9ea883200cec0e53252167df7a413e53bc9a5803
unknown <13776250336@163.com>
2021-05-30 23:57:57 +0800

    2021-5-30

---
+++ 前端课后练习/526直播 移动知乎热榜/zhrb/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <van-search v-model="value" placeholder="请输入搜索关键词" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
